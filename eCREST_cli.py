806093f6a4120ab8f254beb31bbee2bc78be3895
KPerks <4777333+neurologic@users.noreply.github.com>
2023-04-12 10:31:06 -0400

    reconciling campus and home versions

--- eCREST_cli.py
+++ eCREST_cli.py
@@ -1179,4 +1179,31 @@ def import_settings(dict_json):
         settings_dict=myfile.read()
         settings_dict = json.loads(settings_dict)
     return settings_dict
-            
+
+class ScreenshotSaver(object):
+    def __init__(self, viewer, directory):
+        self.viewer = viewer
+        self.directory = directory
+
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
+        self.index = 0
+
+    def get_path(self, index):
+        return os.path.join(self.directory, '%07d.png' % index)
+
+    def get_next_path(self, index=None):
+        if index is None:
+            index = self.index
+        return index, self.get_path(index)
+
+    def capture(self, index=None):
+        s = self.viewer.screenshot()
+        increment_index = index is None
+        index, path = self.get_next_path(index)
+        with open(path, 'wb') as f:
+            f.write(s.screenshot.image)
+        if increment_index:
+            self.index += 1
+        return index, path      
