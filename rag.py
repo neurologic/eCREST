e3578964e2dd6db1520fb76c53f5a806300bbd15
neurologic <4777333+neurologic@users.noreply.github.com>
2024-03-22 08:42:53 -0400

    added a settings_dict json for COLAB runtimes

--- rag.py
+++ rag.py
@@ -12,7 +12,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Utilities for region adjacency graphs (RAGs)."""
+"""Utilities for region adjacency graphs (RAGs).
+from https://github.com/google-research/connectomics/blob/main/connectomics/segmentation/rag.py
+"""
+
 
 import networkx as nx
 import numpy as np
